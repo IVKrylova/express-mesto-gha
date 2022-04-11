d02e938186b4ef2d530bc84e04f797a3e8539393
IVKrylova <ivk.lis@yandex.ru>
2022-04-11 19:45:03 +0300

    refactor: take out NotFoundError in separate file

--- utils/utils.js
+++ utils/utils.js
@@ -1,22 +1,7 @@
 const INTERNAL_SERVER_ERROR_CODE = 500;
 const BAD_REQUEST_CODE = 400;
 const NOT_FOUND_CODE = 404;
-
-class BadRequestError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "BadRequestError";
-    this.statusCode = 400;
-  }
-}
-
-class NotFoundError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "NotFoundError";
-    this.statusCode = 404;
-  }
-}
+const { NotFoundError } = require('./NotFoundError');
 
 const checkRes = res => {
   if (res === null) {
