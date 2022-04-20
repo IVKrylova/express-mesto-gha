b7fd0c1380a5db2dbf011d148f69f7ca0df959d2
IVKrylova <ivk.lis@yandex.ru>
2022-04-20 22:28:40 +0300

    feat: add error handling for celebrate

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
 const usersRoutes = require('./routes/users');
 const cardsRoutes = require('./routes/cards');
 const {
@@ -33,6 +34,9 @@ app.use(auth);
 
 app.use(usersRoutes);
 app.use(cardsRoutes);
+
+// обработчик ошибок celebrate
+app.use(errors());
 
 // oбработка неправильного пути
 app.use((req, res) => {
