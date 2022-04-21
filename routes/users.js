b88dd99c8c5e9f3b8f2e4981cab3518a329d1f24
IVKrylova <ivk.lis@yandex.ru>
2022-04-21 14:15:10 +0300

    fix: validate ObjectId

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.get('/users/me', getCurrentUser);
 router.get('/users', getUsers);
 router.get('/users/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum(),
+    userId: Joi.string().alphanum().length(24),
   }),
 }), getUser);
 router.patch('/users/me', celebrate({
