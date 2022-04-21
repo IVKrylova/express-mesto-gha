c0b66b14aeae46ec54d8a2b3e5dd3518534b71a0
IVKrylova <ivk.lis@yandex.ru>
2022-04-21 13:19:53 +0300

    fix: send json into login

--- controllers/users.js
+++ controllers/users.js
@@ -76,7 +76,7 @@ module.exports.login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
       })
-        .end();
+        .send(user);
     })
     .catch(next);
 };
