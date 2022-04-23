80e923980f21151efad501e246288a842115ff30
IVKrylova <ivk.lis@yandex.ru>
2022-04-23 22:49:58 +0300

    fix: change validation into routes

--- routes/users.js
+++ routes/users.js
@@ -12,18 +12,18 @@ router.get('/users/me', getCurrentUser);
 router.get('/users', getUsers);
 router.get('/users/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24),
+    userId: Joi.string().length(24).hex().required(),
   }),
 }), getUser);
 router.patch('/users/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
   }),
 }), updateProfile);
 router.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().uri()
+    avatar: Joi.string().uri().required()
       .regex(/https?:\/\/(www.)?[\w\-.~:/?#[\]@!$&'()*+,;=]{1,256}\.[a-z0-9]{2,6}\b([-\w()@:%.+~#=//?&]*)/),
   }),
 }), updateAvatar);
