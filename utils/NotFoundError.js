f599db19cbe3c8e68a1c9dcbb1d6d290964b347c
IVKrylova <ivk.lis@yandex.ru>
2022-04-24 13:52:30 +0300

    fix: hendle error

--- utils/NotFoundError.js
+++ utils/NotFoundError.js
@@ -1,11 +1,9 @@
-const { NOT_FOUND_CODE } = require('./utils');
-
 // класс ошибки 404
 class NotFoundError extends Error {
   constructor(message) {
     super(message);
     this.name = 'NotFoundError';
-    this.statusCode = NOT_FOUND_CODE;
+    this.statusCode = 404;
   }
 }
 
