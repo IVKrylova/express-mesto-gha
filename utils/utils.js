a4e164a949d671c0c97bd55619652b95a20cd522
IVKrylova <ivk.lis@yandex.ru>
2022-04-24 17:19:58 +0300

    fix: dalete BAD_REQUEST_CODE

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,4 @@
-const BAD_REQUEST_CODE = 400;
+/* const BAD_REQUEST_CODE = 400; */
 const UNAUTHORIZED_CODE = 401;
 const FORBIDDEN_CODE = 403;
 const CONFLICT_CODE = 409;
@@ -13,7 +13,7 @@ const checkRes = (res) => {
 };
 
 module.exports = {
-  BAD_REQUEST_CODE,
+  /* BAD_REQUEST_CODE, */
   UNAUTHORIZED_CODE,
   FORBIDDEN_CODE,
   CONFLICT_CODE,
