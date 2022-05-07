903df729ada7f01856e72add606881d8084798ea
IVKrylova <ivk.lis@yandex.ru>
2022-05-07 14:21:52 +0300

    fix: add change message by authorization

--- controllers/users.js
+++ controllers/users.js
@@ -120,7 +120,7 @@ module.exports.login = (req, res, next) => {
         sameSite: 'none',
         secure: true,
       })
-        .send({ message: 'Логин успешный', idUser: user._id });
+        .send({ message: 'Логин успешный', user });
     })
     .catch(next);
 };
