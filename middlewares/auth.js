fc6cd6ef303f6b03d27cb1b1aec2f48112c00f75
IVKrylova <ivk.lis@yandex.ru>
2022-05-08 13:02:51 +0300

    fix: make token authentication

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,9 +3,33 @@ const jwt = require('jsonwebtoken');
 const { UnauthorizedError } = require('../utils/UnauthorizedError');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  /* const token = req.cookies.jwt; */
+  // достаём авторизационный заголовок
+  const { authorization } = req.headers;
 
+  // убеждаемся, что он есть или начинается с Bearer
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+
+  // извлечём токен
+  const token = authorization.replace('Bearer ', '');
+
+  // верифицируем токен
+  let payload;
   try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+
+  // записываем пейлоуд в объект запроса
+  req.user = payload;
+
+  next();
+};
+
+/* try {
     const user = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
 
     req.user = user;
@@ -13,4 +37,4 @@ module.exports = (req, res, next) => {
     throw new UnauthorizedError('Необходима авторизация');
   }
   next();
-};
+}; */
